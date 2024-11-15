26ce9b94b5544fa96cf50a29da40bf0b7959d857
Kau Khai Kit <74185102+exellaz@users.noreply.github.com>
2024-11-15 16:59:01 +0800

    feat: added constructor, destructor and getters

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -6,7 +6,59 @@
 /*   By: kkhai-ki <kkhai-ki@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 16:11:19 by kkhai-ki          #+#    #+#             */
-/*   Updated: 2024/11/15 16:11:22 by kkhai-ki         ###   ########.fr       */
+/*   Updated: 2024/11/15 16:58:39 by kkhai-ki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Account.hpp"
+#include <iostream>
+#include <iomanip>
+
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
+
+Account::Account( int initial_deposit )
+		:_accountIndex(Account::_nbAccounts),
+		_amount(initial_deposit),
+		_nbDeposits(0),
+		_nbWithdrawals(0)
+{
+	this->_nbAccounts++;
+	this->_totalAmount += initial_deposit;
+	_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";";
+	std::cout << "amount:" << this->_amount << ";";
+	std::cout << "created;" << std::endl;
+}
+
+Account::~Account( void ) {
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";";
+	std::cout << "amount:" << this->_amount << ";";
+	std::cout << "closed" << std::endl;
+}
+
+int	Account::getNbAccounts( void ) {
+	return Account::_nbAccounts;
+}
+
+int	Account::getTotalAmount( void ) {
+	return Account::_totalAmount;
+}
+
+int	Account::getNbDeposits( void ) {
+	return Account::_totalNbDeposits;
+}
+
+int	Account::getNbWithdrawals( void ) {
+	return Account::_totalNbWithdrawals;
+}
+
+void	Account::_displayTimestamp( void ) {
+	time_t	now = time(0);
+	struct tm	*tm = localtime(&now);
+
+	std::cout << std::put_time(tm, "[%Y%m%d_%H%M%S] ");
+}
