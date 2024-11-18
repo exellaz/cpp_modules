25bd7a8015b811bfce7a58c56ea948d322fc96ad
Kau Khai Kit <74185102+exellaz@users.noreply.github.com>
2024-11-18 13:22:22 +0800

    feat: added randomChump
    - ex00 complete.

--- cpp01/ex00/randomChump.cpp
+++ cpp01/ex00/randomChump.cpp
@@ -1,24 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   randomChump.cpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kkhai-ki <kkhai-ki@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/18 12:47:50 by kkhai-ki          #+#    #+#             */
-/*   Updated: 2024/11/18 13:13:24 by kkhai-ki         ###   ########.fr       */
+/*   Created: 2024/11/18 13:02:40 by kkhai-ki          #+#    #+#             */
+/*   Updated: 2024/11/18 13:12:45 by kkhai-ki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-int	main(void)
+void	randomChump(std::string name)
 {
-	Zombie	*aZombie;
-
-	aZombie = newZombie("Bread");
-	aZombie->announce();
-	randomChump("Random Fish");
-	delete aZombie;
-	return (0);
+	Zombie randomChump(name);
+	randomChump.announce();
 }
