52f723c44deef87167a098adf75b00193ddb06af
Kau Khai Kit <74185102+exellaz@users.noreply.github.com>
2024-11-20 13:17:21 +0800

    feat: added Weapon class

--- cpp01/ex03/Weapon.cpp
+++ cpp01/ex03/Weapon.cpp
@@ -6,7 +6,27 @@
 /*   By: kkhai-ki <kkhai-ki@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 12:52:36 by kkhai-ki          #+#    #+#             */
-/*   Updated: 2024/11/20 12:52:37 by kkhai-ki         ###   ########.fr       */
+/*   Updated: 2024/11/20 13:13:42 by kkhai-ki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Weapon.hpp"
+
+Weapon::Weapon(std::string type)
+	: _type(type)
+{
+}
+
+Weapon::~Weapon(void)
+{
+}
+
+const std::string	&Weapon::getType(void) const
+{
+	return (this->_type);
+}
+
+void	Weapon::setType(std::string type)
+{
+	this->_type = type;
+}
--- cpp01/ex03/Weapon.hpp
+++ cpp01/ex03/Weapon.hpp
@@ -6,7 +6,25 @@
 /*   By: kkhai-ki <kkhai-ki@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 12:52:38 by kkhai-ki          #+#    #+#             */
-/*   Updated: 2024/11/20 12:52:40 by kkhai-ki         ###   ########.fr       */
+/*   Updated: 2024/11/20 13:10:57 by kkhai-ki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef WEAPON_HPP
+# define WEAPON_HPP
+
+# include <string>
+
+class Weapon
+{
+	private:
+		std::string	_type;
+
+	public:
+		Weapon(std::string type);
+		~Weapon(void);
+		const std::string	&getType(void) const;
+		void				setType(std::string type);
+};
+
+#endif
