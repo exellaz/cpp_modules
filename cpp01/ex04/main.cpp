5216e34217c54eafdb0e21c9e07b2824a3aee91b
Kau Khai Kit <74185102+exellaz@users.noreply.github.com>
2024-11-21 13:11:30 +0800

    style: seperated parts of main into its own functions

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: kkhai-ki <kkhai-ki@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 15:11:38 by kkhai-ki          #+#    #+#             */
-/*   Updated: 2024/11/21 12:27:12 by kkhai-ki         ###   ########.fr       */
+/*   Updated: 2024/11/21 13:02:49 by kkhai-ki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,19 @@
 #include <sstream>
 #include <iostream>
 
-std::string	replace_str(std::string initial, std::string toReplace, std::string replaceWith)
+static bool	validate_args(int ac, char *av[]);
+static std::string	replace_str(std::string initial, std::string toReplace, std::string replaceWith);
+static void	read_and_replace(char *av[]);
+
+int	main(int ac, char *av[])
+{
+	if (!validate_args(ac, av))
+		return (1);
+	read_and_replace(av);
+	return (0);
+}
+
+static std::string	replace_str(std::string initial, std::string toReplace, std::string replaceWith)
 {
 	size_t		i = 0;
 	size_t		found;
@@ -35,28 +47,34 @@ std::string	replace_str(std::string initial, std::string toReplace, std::string 
 	return (result);
 }
 
-int	main(int ac, char *av[])
+static bool	validate_args(int ac, char *av[])
 {
+	std::ifstream	file_read;
+
 	if (ac != 4)
 	{
 		std::cout << "Error: expected: ./fake_sed <filename> <toReplace> <replaceWith>" << std::endl;
-		return (1);
+		return (false);
 	}
-	std::string			filename = av[1];
-	std::string			toReplace = av[2];
-	std::string			replaceWith = av[3];
-	std::string			content;
-	std::ostringstream	buffer;
-	std::ifstream		file_read(filename.c_str());
-	std::ofstream		file_write;
+	file_read.open(av[1]);
 	if (file_read.is_open() == false)
 	{
 		std::cout << "Error: Invalid file" << std::endl;
-		return (1);
+		return (false);
 	}
-	file_write.open(filename.append(".replace").c_str());
-	buffer << file_read.rdbuf();
-	content = replace_str(buffer.str(), toReplace, replaceWith);
-	file_write << content;
-	return (0);
+	file_read.close();
+	return (true);
+}
+
+static void	read_and_replace(char *av[])
+{
+	const std::string	&filename(av[1]);
+	std::ifstream		read_file;
+	std::ofstream		write_file;
+	std::ostringstream	buffer;
+
+	read_file.open(filename.c_str());
+	write_file.open((filename + ".replace").c_str());
+	buffer << read_file.rdbuf();
+	write_file << replace_str(buffer.str(), av[2], av[3]);
 }
