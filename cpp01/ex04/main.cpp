ee3e653dfdcea18e7725bd6ddcab1a265a4c2489
Kau Khai Kit <74185102+exellaz@users.noreply.github.com>
2024-11-21 12:28:38 +0800

    fix: fixed a bug where  gets appended even without one

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,12 +6,13 @@
 /*   By: kkhai-ki <kkhai-ki@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 15:11:38 by kkhai-ki          #+#    #+#             */
-/*   Updated: 2024/11/21 11:50:42 by kkhai-ki         ###   ########.fr       */
+/*   Updated: 2024/11/21 12:27:12 by kkhai-ki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fstream>
 #include <string>
+#include <sstream>
 #include <iostream>
 
 std::string	replace_str(std::string initial, std::string toReplace, std::string replaceWith)
@@ -41,21 +42,21 @@ int	main(int ac, char *av[])
 		std::cout << "Error: expected: ./fake_sed <filename> <toReplace> <replaceWith>" << std::endl;
 		return (1);
 	}
-	std::string		filename = av[1];
-	std::string		toReplace = av[2];
-	std::string		replaceWith = av[3];
-	std::string		content;
-	std::string		line;
-	std::ifstream	file_read(filename.c_str());
+	std::string			filename = av[1];
+	std::string			toReplace = av[2];
+	std::string			replaceWith = av[3];
+	std::string			content;
+	std::ostringstream	buffer;
+	std::ifstream		file_read(filename.c_str());
+	std::ofstream		file_write;
 	if (file_read.is_open() == false)
 	{
 		std::cout << "Error: Invalid file" << std::endl;
 		return (1);
 	}
-	std::ofstream file_output(filename.append(".replace").c_str());
-	while (std::getline(file_read, line))
-		content.append(line).append("\n");
-	content = replace_str(content, toReplace, replaceWith);
-	file_output << content;
+	file_write.open(filename.append(".replace").c_str());
+	buffer << file_read.rdbuf();
+	content = replace_str(buffer.str(), toReplace, replaceWith);
+	file_write << content;
 	return (0);
 }
