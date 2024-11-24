86bb3def3216e2c32f4db4ea59d83b02fbcb4114
Kau Khai Kit <74185102+exellaz@users.noreply.github.com>
2024-11-24 15:44:27 +0800

    ex02: added arithmetic operators

---
+++ cpp02/ex02/Fixed.cpp
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkhai-ki <kkhai-ki@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/23 21:40:59 by kkhai-ki          #+#    #+#             */
+/*   Updated: 2024/11/24 15:37:10 by kkhai-ki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+Fixed::Fixed(void)
+	: _raw_bits(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::~Fixed(void)
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &other)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	setRawBits(other.getRawBits());
+}
+
+Fixed::Fixed(const int num)
+	: _raw_bits(num << _bits)
+{
+	std::cout << "Int constructor called" << std::endl;
+}
+
+Fixed::Fixed(const float num)
+	: _raw_bits(roundf(num * (1 << _bits)))
+{
+	std::cout << "Float constructor called" << std::endl;
+}
+
+//Return value here allows for chaining of assignment operation (e.g. a = b = c;)
+Fixed	&Fixed::operator = (const Fixed &other)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &other)
+		setRawBits(other.getRawBits());
+	return (*this);
+}
+
+int	Fixed::getRawBits(void) const
+{
+	return (_raw_bits);
+}
+
+void	Fixed::setRawBits(const int raw)
+{
+	_raw_bits = raw;
+}
+
+float	Fixed::toInt(void) const
+{
+	return (_raw_bits >> _bits);
+}
+
+float	Fixed::toFloat(void) const
+{
+	return ((float)_raw_bits / (float)(1 << _bits));
+}
+
+Fixed	Fixed::operator+(const Fixed &f) const
+{
+	Fixed	result(this->toFloat() + f.toFloat());
+	return (result);
+}
+
+Fixed	Fixed::operator-(const Fixed &f) const
+{
+	Fixed	result(this->toFloat() - f.toFloat());
+	return (result);
+}
+
+Fixed	Fixed::operator*(const Fixed &f) const
+{
+	Fixed	result(this->toFloat() * f.toFloat());
+	return (result);
+}
+
+Fixed	Fixed::operator/(const Fixed &f) const
+{
+	Fixed	result(this->toFloat() / f.toFloat());
+	return (result);
+}
+
+std::ostream	&operator<<(std::ostream &stream, const Fixed &f)
+{
+	stream << f.toFloat();
+	return (stream);
+}
---
+++ cpp02/ex02/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkhai-ki <kkhai-ki@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/24 15:40:00 by kkhai-ki          #+#    #+#             */
+/*   Updated: 2024/11/24 15:41:57 by kkhai-ki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int	main(void)
+{
+	Fixed		a;
+	Fixed const	b(Fixed(5.05f) * Fixed(2));
+
+	std::cout << a << std::endl;
+	// std::cout << ++a << std::endl;
+	// std::cout << a << std::endl;
+	// std::cout << a++ << std::endl;
+	// std::cout << a << std::endl;
+
+	std::cout << b << std::endl;
+
+	// std::cout << Fixed::max( a, b ) << std::endl;
+	return (0);
+}
