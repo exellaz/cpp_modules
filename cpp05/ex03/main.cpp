882b3e71e315e557f9e3083ca52793f90dc0e89f
Kau Khai Kit <74185102+exellaz@users.noreply.github.com>
2025-04-08 08:46:12 +0800

    ex03: change test cases
    Added attributes check test and separated all tests into their own functions

--- cpp05/ex03/main.cpp
+++ cpp05/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: kkhai-ki <kkhai-ki@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 08:41:54 by kkhai-ki          #+#    #+#             */
-/*   Updated: 2025/04/08 08:24:32 by kkhai-ki         ###   ########.fr       */
+/*   Updated: 2025/04/08 08:45:47 by kkhai-ki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,39 +64,37 @@ int	main()
 	return 0;
 }
 
-// bool	test_Intern_makeForm()
-// {
-// 	Intern	intern;
-// 	AForm	*form;
-
-// 	form = intern.makeForm("PresidentialPardonForm", "Amongus");
-// 	form->action();
-// 	return (true);
-// }
-
-bool	test_Intern_MakeForm_Shrubbery()
+bool	test_Intern_makeForm_attributes()
 {
 	std::ostringstream	errors;
 	bool				result = true;
 
 	{
-		const std::string	formName = "ShruberryCreationForm";
-		const std::string	target = "garden";
+		const std::string	expectedName = "ShruberryCreationForm";
+		const std::string	expectedTarget = "greenfield";
+		const int			expectedGradeToSign = 145;
+		const int			expectedGradeToExecute = 137;
+		const bool			expectedSignedStatus = false;
 
 		Intern	intern;
-		AForm	*form = intern.makeForm(formName, target);
+		AForm	*form;
 
-		CHECK(formName, form->getName());
-		CHECK(target, form->getTarget());
-		CHECK(typeid(*form).name(), typeid(ShruberryCreationForm).name());
+		std::cout.rdbuf(NULL);
+		form = intern.makeForm(expectedName, expectedTarget);
+		std::cout.rdbuf(originalCout);
+
+		CHECK(expectedName, form->getName());
+		CHECK(expectedTarget, form->getTarget());
+		CHECK(expectedGradeToSign, form->getGradeToSign());
+		CHECK(expectedGradeToExecute, form->getGradeToExecute());
+		CHECK(expectedSignedStatus, form->getSigned());
 
 		delete form;
 	}
-	REPORT(result, "Intern MakeForm - Shrubbery Form");
+	REPORT(result, "Intern makeForm attribute check test");
 	return (result);
 }
-
-bool	test_Intern_makeForm()
+bool	test_Intern_makeForm_Shruberry()
 {
 	std::ostringstream	errors;
 	bool				result = true;
@@ -118,12 +116,12 @@ bool	test_Intern_makeForm()
 								"   .....//||||\\....";
 		const std::string	expectedException = "";
 
-		Intern					intern;
-		Bureaucrat				bureaucrat("TreeMan", 1);
-		std::string				exceptionCaught;
-		std::ifstream			infile;
-		std::ostringstream		fileContents;
-		AForm					*form;
+		Intern				intern;
+		Bureaucrat			bureaucrat("TreeMan", 1);
+		std::string			exceptionCaught;
+		std::ifstream		infile;
+		std::ostringstream	fileContents;
+		AForm				*form;
 
 		std::cout.rdbuf(NULL);
 		try
@@ -148,16 +146,25 @@ bool	test_Intern_makeForm()
 
 		delete form;
 	}
+	REPORT(result, "Intern makeForm Shruberry test");
+	return (result);
+}
+
+bool	test_Intern_makeForm_Robotomy()
+{
+	std::ostringstream	errors;
+	bool				result = true;
+
 	{
 		const std::string	expectedType = "RobotomyRequestForm";
 		const std::string	expectedTarget = "victim";
 		const std::string	expectedOutput = "Bob couldn't execute RobotomyRequestForm because grade is too low\n";
 
-		Intern					intern;
-		Bureaucrat				bureaucrat("Bob", 46);
-		std::ostringstream		captureOutput;
-		std::string				output;
-		AForm					*form;
+		Intern				intern;
+		Bureaucrat			bureaucrat("Bob", 46);
+		std::ostringstream	captureOutput;
+		std::string			output;
+		AForm				*form;
 
 		std::cout.rdbuf(NULL);
 		form = intern.makeForm(expectedType, expectedTarget);
@@ -173,13 +180,58 @@ bool	test_Intern_makeForm()
 
 		delete form;
 	}
+	REPORT(result, "Intern makeForm Robotomy test");
+	return (result);
+}
+
+bool	test_Intern_makeForm_Presidential()
+{
+	std::ostringstream	errors;
+	bool				result = true;
+
+	{
+		const std::string	expectedType = "PresidentialPardonForm";
+		const std::string	expectedTarget = "FordPrefect";
+		const std::string	expectedOutput = \
+			"FordPrefect has been pardoned by Zaphod Beeblebrox.\n"
+			"ArthurDent executed PresidentialPardonForm\n";
+
+		Intern				intern;
+		Bureaucrat			bureaucrat("ArthurDent", 1);
+		std::ostringstream	captureOutput;
+		std::string			output;
+		AForm				*form;
+
+		std::cout.rdbuf(NULL);
+		form = intern.makeForm(expectedType, expectedTarget);
+		std::cout.rdbuf(captureOutput.rdbuf());
+		form->beSigned(bureaucrat);
+		bureaucrat.executeForm(*form);
+		std::cout.rdbuf(originalCout);
+		output = captureOutput.str();
+
+		CHECK(expectedType, form->getName());
+		CHECK(expectedTarget, form->getTarget());
+		CHECK(expectedOutput, output);
+
+		delete form;
+	}
+	REPORT(result, "Intern makeForm Presidential test");
+	return (result);
+}
+
+bool	test_Intern_makeForm_Invalid()
+{
+	std::ostringstream	errors;
+	bool				result = true;
+
 	{
 		const std::string	expectedOutput = "Cannot find form of InvalidFormName\n";
 
-		Intern					intern;
-		std::ostringstream		captureOutput;
-		std::string				output;
-		AForm					*form;
+		Intern				intern;
+		std::ostringstream	captureOutput;
+		std::string			output;
+		AForm				*form;
 
 		std::cout.rdbuf(captureOutput.rdbuf());
 		form = intern.makeForm("InvalidFormName", "some_target");
@@ -189,10 +241,9 @@ bool	test_Intern_makeForm()
 		CHECK(0, form);
 		CHECK(expectedOutput, output);
 	}
-	REPORT(result, "Intern makeForm test");
+	REPORT(result, "Intern makeForm invalid test");
 	return (result);
 }
-
 
 void	run_tests()
 {
@@ -200,8 +251,11 @@ void	run_tests()
 	int		failed = 0;
 	bool	(*tests[])() = \
 	{
-		test_Intern_makeForm,
-		// test_Intern_MakeForm_Shrubbery,
+		test_Intern_makeForm_attributes,
+		test_Intern_makeForm_Shruberry,
+		test_Intern_makeForm_Robotomy,
+		test_Intern_makeForm_Presidential,
+		test_Intern_makeForm_Invalid,
 	};
 
 	int	num_tests = sizeof(tests) / sizeof(tests[0]);
