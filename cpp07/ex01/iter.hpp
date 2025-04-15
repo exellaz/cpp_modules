01d865192f27ef69df577f88ff9aa3ce1726f84b
Kau Khai Kit <74185102+exellaz@users.noreply.github.com>
2025-04-15 09:52:54 +0800

    ex01: done iter

---
+++ cpp07/ex01/iter.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkhai-ki <kkhai-ki@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/15 09:04:40 by kkhai-ki          #+#    #+#             */
+/*   Updated: 2025/04/15 09:38:35 by kkhai-ki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ITER_HPP
+# define ITER_HPP
+
+# include <iostream>
+
+template <typename T>
+void	iter(T* array, size_t length, void(*f)(const T&))
+{
+	for (size_t i = 0; i < length; i++)
+		f(array[i]);
+}
+
+#endif
---
+++ cpp07/ex01/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkhai-ki <kkhai-ki@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/15 09:14:58 by kkhai-ki          #+#    #+#             */
+/*   Updated: 2025/04/15 09:50:47 by kkhai-ki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "iter.hpp"
+
+template<typename T>
+void	printElement(const T& element)
+{
+	std::cout << element << "\n";
+}
+
+int	main()
+{
+	int			iArr[] = {1, 2, 3, 4, 5};
+	char		charArr[] = {'H','e','l','l','o','\0'};
+	std::string	strArr[] = {"Hello", "There"};
+
+	iter(iArr, 5, printElement);
+	iter(charArr, 5, printElement);
+	iter(strArr, 2, printElement);
+
+	return 0;
+}
