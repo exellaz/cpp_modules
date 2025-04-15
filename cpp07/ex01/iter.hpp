3fb17b3ce6d7ada54f7290bb0e399f219b11049c
Kau Khai Kit <74185102+exellaz@users.noreply.github.com>
2025-04-15 10:47:31 +0800

    ex01: add overload to accept parameters that are not const

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: kkhai-ki <kkhai-ki@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 09:04:40 by kkhai-ki          #+#    #+#             */
-/*   Updated: 2025/04/15 09:38:35 by kkhai-ki         ###   ########.fr       */
+/*   Updated: 2025/04/15 10:31:10 by kkhai-ki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,13 @@
 # define ITER_HPP
 
 # include <iostream>
+
+template <typename T>
+void	iter(T* array, size_t length, void(*f)(T&))
+{
+	for (size_t i = 0; i < length; i++)
+		f(array[i]);
+}
 
 template <typename T>
 void	iter(T* array, size_t length, void(*f)(const T&))
