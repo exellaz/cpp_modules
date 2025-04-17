09a20d3600bfc47c1fc8bfbea29925afb3db3029
Kau Khai Kit <74185102+exellaz@users.noreply.github.com>
2025-04-17 11:05:14 +0800

    ex02: clean up deprecated code

--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -6,25 +6,17 @@
 /*   By: kkhai-ki <kkhai-ki@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 21:14:43 by kkhai-ki          #+#    #+#             */
-/*   Updated: 2025/04/16 22:35:59 by kkhai-ki         ###   ########.fr       */
+/*   Updated: 2025/04/17 11:04:29 by kkhai-ki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include <iostream>
-// #include "Array.hpp"
-// #define GREEN "\033[32m"
-// #define BLUE "\033[34m"
-// #define RESET "\033[0m"
+void	run_tests();
 
-
-// int	main()
-// {
-// 	Array<int>	emptyArr;
-// 	std::cout << BLUE << "emptyArr size: " << emptyArr.size() << RESET << "\n";
-
-
-// 	return 0;
-// }
+int main()
+{
+	run_tests();
+	return 0;
+}
 #include <iostream>
 #include "Array.hpp"
 
@@ -34,13 +26,13 @@
 #define BLUE    "\033[34m"
 #define RESET   "\033[0m"
 
-void test_default_constructor() {
+void test_defaultConstructor() {
 	std::cout << YELLOW << "[Test] Default Constructor\n" << RESET;
 	Array<int> arr;
 	std::cout << "Size: " << arr.size() << std::endl;
 }
 
-void test_constructor_with_size() {
+void test_parameterizedConstructor() {
 	std::cout << YELLOW << "[Test] Constructor with Size\n" << RESET;
 	Array<int> arr(5);
 	for (unsigned int i = 0; i < arr.size(); i++)
@@ -50,7 +42,7 @@ void test_constructor_with_size() {
 		std::cout << "arr[" << i << "] = " << arr[i] << std::endl;
 }
 
-void test_copy_constructor() {
+void test_copyConstructor() {
 	std::cout << YELLOW << "[Test] Copy Constructor\n" << RESET;
 	Array<std::string> original(2);
 	original[0] = "Hello";
@@ -61,7 +53,7 @@ void test_copy_constructor() {
 		std::cout << "copy[" << i << "] = " << copy[i] << std::endl;
 }
 
-void test_copy_assignment() {
+void test_copyAssignment() {
 	std::cout << YELLOW << "[Test] Copy Assignment\n" << RESET;
 	Array<float> a(3);
 	a[0] = 1.1f;
@@ -74,7 +66,7 @@ void test_copy_assignment() {
 		std::cout << "b[" << i << "] = " << b[i] << std::endl;
 }
 
-void test_bounds_checking() {
+void test_boundsChecking() {
 	std::cout << YELLOW << "[Test] Bounds Checking\n" << RESET;
 	Array<int> arr(3);
 	try {
@@ -88,11 +80,11 @@ void	run_tests()
 {
 	void	(*tests[])() = \
 	{
-		test_default_constructor,
-		test_constructor_with_size,
-		test_copy_constructor,
-		test_copy_assignment,
-		test_bounds_checking
+		test_defaultConstructor,
+		test_parameterizedConstructor,
+		test_copyConstructor,
+		test_copyAssignment,
+		test_boundsChecking
 	};
 
 	int	numTests = sizeof(tests) / sizeof(tests[0]);
@@ -103,24 +95,4 @@ void	run_tests()
 		tests[i]();
 		std::cout << BLUE << "-------------------------------------------" << RESET << std::endl;
 	}
-}
-
-int main() {
-	// test_default_constructor();
-	// std::cout << BLUE << "---------------------------\n" << RESET;
-
-	// test_constructor_with_size();
-	// std::cout << BLUE << "---------------------------\n" << RESET;
-
-	// test_copy_constructor();
-	// std::cout << BLUE << "---------------------------\n" << RESET;
-
-	// test_copy_assignment();
-	// std::cout << BLUE << "---------------------------\n" << RESET;
-
-	// test_bounds_checking();
-	// std::cout << BLUE << "---------------------------\n" << RESET;
-	run_tests();
-
-	return 0;
 }
