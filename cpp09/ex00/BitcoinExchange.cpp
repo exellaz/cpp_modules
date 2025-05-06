3892f541ca15a7e74aaecbe8b12aea6bd1fbb516
Kau Khai Kit <74185102+exellaz@users.noreply.github.com>
2025-05-06 14:36:31 +0800

    ex00: add validation of arguments in main

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -3,12 +3,128 @@
 BitcoinExchange::BitcoinExchange()
     : _database()
 {
-
 }
 
 BitcoinExchange::~BitcoinExchange()
 {
+}
 
+std::map<std::string, float> BitcoinExchange::loadDatabase(const std::string& filename)
+{
+    std::map<std::string, float> database;
+
+    std::ifstream file(filename.c_str());
+    if (!file.is_open()) {
+        std::cerr << "Error: could not open database file\n";
+        return database;
+    }
+
+    std::string header;
+    std::getline(file, header);
+    if (header != "date,exchange_rate") {
+        std::cerr << "Error: incorrect header\n";
+        return database;
+    }
+
+    std::string line;
+    while (std::getline(file, line)) {
+        std::string date;
+        float price;
+        if (!parseLine(line, date, price)) {
+            std::cerr << "Error: invalid line format: `" << line << "`\n";
+            continue;
+        }
+        database[date] = price;
+    }
+    return database;
+}
+
+void BitcoinExchange::processInput(const std::string& filename)
+{
+    std::ifstream file(filename.c_str());
+    if (!file.is_open()) {
+        std::cerr << "Error: could not open input file\n";
+        return ;
+    }
+
+    std::string header;
+    std::getline(file, header);
+    if (header != "date | value") {
+        std::cout << header << "\n";
+        std::cerr << "Error: incorrect header\n";
+        return ;
+    }
+
+    std::string line;
+    while (std::getline(file, line)) {
+        std::istringstream iss(line);
+        std::string date;
+        std::getline(iss, date, '|');
+        if (!isValidDate(date)) {
+            std::cout << "Error: bad input => " << date << "\n";
+            continue ;
+        }
+
+        std::string valueStr;
+        float value;
+        std::getline(iss, valueStr);
+        if (!validateValue(valueStr, &value)) {
+            continue ;
+        }
+
+        std::map<std::string, float>::iterator it = findClosestDate(_database, date);
+        std::cout << date << " => " << value << " = " << value * it->second << "\n";
+    }
+}
+
+void BitcoinExchange::trim(std::string& str)
+{
+    size_t first = str.find_first_not_of(" \t\r\n");
+    if (first == std::string::npos) {
+        str = "";
+        return;
+    }
+
+    size_t last = str.find_last_not_of(" \t\r\n");
+    str = str.substr(first, (last - first + 1));
+}
+
+bool BitcoinExchange::validateValue(std::string& valueStr, float* value)
+{
+    trim(valueStr);
+    if (valueStr[0] == '\0') {
+        std::cout << "Error: no number given.\n";
+        return false;
+    }
+    if (valueStr[0] == '-') {
+        std::cout << "Error: not a positive number.\n";
+        return false;
+    }
+
+    char *endptr;
+    *value = std::strtof(valueStr.c_str(), &endptr);
+    while (*endptr && std::isspace(*endptr))
+        ++endptr;
+
+    if (*endptr != '\0') {
+        std::cout << "Error: bad input => " << valueStr << "\n";
+        return false;
+    }
+    if (*value > 1000.0f) {
+        std::cout << "Error: too large a number.\n";
+        return false;
+    }
+
+    return true;
+}
+
+std::map<std::string, float>::iterator BitcoinExchange::findClosestDate(
+    std::map<std::string, float>& database, std::string& target)
+{
+    std::map<std::string, float>::iterator it = database.lower_bound(target);
+    if ((it != database.end() && it->first == target) || it == database.begin())
+        return it;
+    return --it;
 }
 
 bool BitcoinExchange::isValidDate(std::string& date)
@@ -60,34 +176,4 @@ bool BitcoinExchange::parseLine(const std::string& line, std::string& date, floa
         return false;
 
     return true;
-}
-
-std::map<std::string, float> BitcoinExchange::loadDatabase(const std::string& filename)
-{
-    std::map<std::string, float> database;
-    std::ifstream file(filename.c_str());
-
-    if (!file.is_open()) {
-        std::cerr << "Error: could not open database file\n";
-        return database;
-    }
-
-    std::string header;
-    std::getline(file, header);
-    if (header != "date,exchange_rate") {
-        std::cerr << "Error: incorrect header\n";
-        return database;
-    }
-
-    std::string line;
-    while (std::getline(file, line)) {
-        std::string date;
-        float price;
-        if (!parseLine(line, date, price)) {
-            std::cerr << "Error: invalid line format: `" << line << "`\n";
-            continue;
-        }
-        database[date] = price;
-    }
-    return database;
 }
--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -1,100 +1,14 @@
 #include "BitcoinExchange.hpp"
 
-void BitcoinExchange::trim(std::string& str)
+int main(int argc, char *argv[])
 {
-    size_t first = str.find_first_not_of(" \t\r\n");
-    if (first == std::string::npos) {
-        str = "";
-        return;
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <input_file>\n";
+        return 1;
     }
 
-    size_t last = str.find_last_not_of(" \t\r\n");
-    str = str.substr(first, (last - first + 1));
-}
-
-bool BitcoinExchange::validateValue(std::string& valueStr, float* value)
-{
-    trim(valueStr);
-    if (valueStr[0] == '\0') {
-        std::cout << "Error: no number given.\n";
-        return false;
-    }
-    if (valueStr[0] == '-') {
-        std::cout << "Error: not a positive number.\n";
-        return false;
-    }
-
-    char *endptr;
-    *value = std::strtof(valueStr.c_str(), &endptr);
-    while (*endptr && !std::isspace(*endptr))
-        ++endptr;
-
-    if (*endptr != '\0') {
-        std::cout << "Error: bad input => " << valueStr << "\n";
-        return false;
-    }
-
-    if (*value > 1000.0f) {
-        std::cout << "Error: too large a number\n";
-        return false;
-    }
-
-    return true;
-}
-
-std::map<std::string, float>::iterator BitcoinExchange::findClosestDate(
-    std::map<std::string, float>& database, std::string& target)
-{
-    std::map<std::string, float>::iterator it = database.lower_bound(target);
-    if ((it != database.end() && it->first == target) || it == database.begin())
-        return it;
-    return --it;
-}
-
-void BitcoinExchange::processInput(const std::string& filename)
-{
-    std::ifstream ss(filename.c_str());
-
-    std::string header;
-    std::getline(ss, header);
-    if (header != "date | value") {
-        std::cout << header << "\n";
-        std::cerr << "Error: incorrect header\n";
-        return ;
-    }
-
-    std::string line;
-    while (std::getline(ss, line)) {
-        std::istringstream iss(line);
-        std::string date;
-        std::getline(iss, date, '|');
-        if (!isValidDate(date)) {
-            std::cout << "Error: bad input => " << date << "\n";
-            continue ;
-        }
-
-        std::string valueStr;
-        float value;
-        std::getline(iss, valueStr);
-        if (!validateValue(valueStr, &value)) {
-            continue ;
-        }
-
-        std::map<std::string, float>::iterator it = findClosestDate(_database, date);
-        std::cout << date << " => " << value << " = " << value * it->second << "\n";
-    }
-}
-
-int main()
-{
     BitcoinExchange btc;
-
     btc._database = btc.loadDatabase("data.csv");
-    btc.processInput("input.txt");
-    // std::map<std::string, float> database = BitcoinExchange::loadDatabase("small-data.csv");
-    // for (std::map<std::string, float>::iterator it = btc._database.begin(); it != btc._database.end(); ++it) {
-    //     std::cout << "Date: " << it->first << " | Price: " << it->second << "\n";
-    // }
-
+    btc.processInput(argv[1]);
     return 0;
 }
