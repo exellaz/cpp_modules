da39e4ddf62ae1271a97e83ca8f3e7c5cd58aecc
Kau Khai Kit <74185102+exellaz@users.noreply.github.com>
2025-05-16 13:29:53 +0800

    ex02: refactor mergeInsertionSort

--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -20,163 +20,46 @@ class PmergeMe
         PmergeMe& operator=(const PmergeMe& src);
         PmergeMe(const PmergeMe& src);
 
-        template<typename T>
-        void	printContainer(T& container)
-        {
-            typename T::iterator lastComma = --container.end();
-
-            std::cout << "{";
-            for (typename T::iterator it = container.begin(); it != container.end(); ++it) {
-                std::cout << *it;
-                if (it != lastComma)
-                    std::cout << ",";
-            }
-            std::cout << "}\n";
-        }
-
-        static bool isPositiveInteger(const std::string& str);
-        static void trim(std::string& str);
+        bool isPositiveInteger(const std::string& str);
+        void trim(std::string& str);
         void initVector(char* av[]);
-        static int jacobsthalNumber(int n);
+        int jacobsthalNumber(int n);
 
-        template<typename T> T next(T it, int steps);
-        template<typename T> void mergeInsertionSort(T& container, int depth);
-        template<typename T, typename Iterator> void sortGroups(T& container, Iterator& end, int depth);
-        template<typename T> static bool compare(T a, T b);
-        template<typename T> void swapElements(T it, int depth);
+        template<typename T>
+        void mergeInsertionSort(T& container, int groupSize);
+
+        template<typename T, typename Iterator>
+        void sortGroups(T& container, Iterator& end, int groupSize);
+
+        template<typename T, typename ItVec>
+        void initMainAndPend(T& container, ItVec& main, ItVec& pend, int groupSize, int numOfGroups);
+
+        template<typename Iterator, typename ItVec>
+        void insertJacobsthalGroups(ItVec& main, ItVec& pend);
+
+        template<typename Iterator, typename ItVec>
+        void insertRemainingPend(ItVec& main, ItVec& pend, bool isOdd);
+
+        template<typename Iterator, typename T, typename ItVec>
+        void reconstructSortedContainer(T& container, ItVec& main, int groupSize);
+
+        template<typename T>
+        void swapElements(T it, int groupSize);
+
+        template<typename T>
+        T next(T it, int steps);
+
+        template<typename T>
+        bool compare(T a, T b);
 
         private:
+
 };
 
-template<typename T> bool PmergeMe::compare(T a, T b)
-{
-    return *a < *b;
-}
+template<typename T>
+void printContainer(T& container);
 
-template<typename T> void PmergeMe::swapElements(T it, int depth)
-{
-    T start = next(it, -depth + 1);
-    T end = next(start, depth);
-    while (start != end) {
-        std::iter_swap(start, next(start, depth));
-        start++;
-    }
-}
+# include "PmergeMe.tpp"
 
-template<typename T> T PmergeMe::next(T it, int steps)
-{
-    std::advance(it, steps);
-    return it;
-}
-
-template<typename T, typename Iterator> void PmergeMe::sortGroups(T& container, Iterator& end, int depth)
-{
-    int step = 2 * depth;
-    for (Iterator it = container.begin(); it != end; std::advance(it, step)) {
-        Iterator a = next(it, depth - 1);
-        Iterator b = next(it, depth * 2 - 1);
-        if (!compare(a, b)) // Number of comparisons here is always once per group for each recursion
-            swapElements(a, depth);
-    }
-}
-
-// Depth also corresponds to the amount of elements per group
-template<typename T> void PmergeMe::mergeInsertionSort(T& container, int depth)
-{
-    typedef typename T::iterator Iterator;
-
-    int numOfGroups = container.size() / depth;
-    if (numOfGroups < 2)
-        return;
-
-    bool isOdd = numOfGroups % 2 == 1; // Number of groups is odd
-    Iterator lastGroup = next(container.begin(), depth * numOfGroups); // End iterator of last group
-    Iterator end = next(lastGroup, -(isOdd * depth)); // Start iterator of last group
-
-    sortGroups(container, end, depth);
-    mergeInsertionSort(container, depth * 2);
-
-    // printContainer(container);
-    // std::cout << "Depth: " << depth << "\n";
-
-    std::vector<Iterator> main;
-    std::vector<Iterator> pend;
-
-    // std::cout << "b1: " << *(next(container.begin(), depth - 1)) << "\n";
-    main.push_back(next(container.begin(), depth - 1)); // Push b1
-    // std::cout << "a1: " << *(next(container.begin(), depth * 2 - 1)) << "\n";
-    main.push_back(next(container.begin(), depth * 2 - 1)); // Push a1
-
-    for (int i = 3; i <= numOfGroups; i++) {
-        if (i % 2 == 1)
-            pend.push_back(next(container.begin(), depth * i - 1));
-        else
-            main.push_back(next(container.begin(), depth * i - 1));
-    }
-
-    // std::cout << "Pend: " ;
-    // for (int i = 0; i < static_cast<int>(pend.size()); i++) {
-    //     std::cout << *(pend[i]) << " ";
-    // }
-    // std::cout << "\n";
-    // std::cout << "Main: " ;
-    // for (int i = 0; i < static_cast<int>(main.size()); i++) {
-    //     std::cout << *(main[i]) << " ";
-    // }
-    // std::cout << "\n";
-
-    int prevJacobsthal = jacobsthalNumber(1);
-    // insertedNumbers only updates after all of the numbers from each group
-    // has been inserted. Used to define the boundary for insertion
-    int insertedNumbers = 0;
-    for (int k = 2;; k++) {
-        int currJacobsthal = jacobsthalNumber(k);
-        int jacobsthalDiff = currJacobsthal - prevJacobsthal;
-        int offset = 0;
-        if (jacobsthalDiff > static_cast<int>(pend.size()))
-            break;
-        // pendIt is the end Iterator of the current group
-        typename std::vector<Iterator>::iterator pendIt = next(pend.begin(), jacobsthalDiff - 1);
-        // boundIt is the bound for the Binary Insertion in the main chain
-        typename std::vector<Iterator>::iterator boundIt = next(main.begin(), currJacobsthal + insertedNumbers);
-        for (int nbrOfInsertions = 0; nbrOfInsertions < jacobsthalDiff; nbrOfInsertions++) {
-            //upper_bound uses Binary Insertion, passing it the compare function overloads the comparator
-            typename std::vector<Iterator>::iterator idx = std::upper_bound(main.begin(), boundIt, *pendIt, compare<Iterator>);
-            typename std::vector<Iterator>::iterator inserted = main.insert(idx, *pendIt);
-            pend.erase(pendIt); // Remove the inserted iterator from pend
-            std::advance(pendIt, -1); // Move the iterator backwards
-
-            // Offset here is to adjust the boundary by reducing it when the current iterator is
-            // inserted before the next iterator
-            offset += (inserted - main.begin()) == currJacobsthal + insertedNumbers;
-            // The boundary is adjusted by the above offset and the insertedNumbers
-            boundIt = next(main.begin(), currJacobsthal + insertedNumbers - offset);
-        }
-        prevJacobsthal = currJacobsthal;
-        insertedNumbers += jacobsthalDiff;
-    }
-
-    // This will be used to insert
-    for (int i = pend.size() - 1; i >= 0; i--) {
-        typename std::vector<Iterator>::iterator currPend = next(pend.begin(), i);
-        typename std::vector<Iterator>::iterator currBound = next(main.begin(), main.size() - pend.size() + i + isOdd);
-        // std::cout << "currBound: " << currBound - main.begin() << "\n";
-        typename std::vector<Iterator>::iterator idx = std::upper_bound(main.begin(), currBound, *currPend, compare<Iterator>);
-        // std::cout << "idx: " << *(*currPend) << "\n";
-        main.insert(idx, *currPend);
-    }
-
-    std::vector<int> copy;
-    copy.reserve(container.size());
-    for (typename std::vector<Iterator>::iterator it = main.begin(); it != main.end(); it++) {
-        for (int i = 0; i < depth; i++) {
-            Iterator pairStart = *it;
-            std::advance(pairStart, -depth + i + 1);
-            copy.insert(copy.end(), *pairStart);
-        }
-    }
-
-    std::copy(copy.begin(), copy.end(), container.begin());
-}
 
 #endif
---
+++ cpp09/ex02/PmergeMe.tpp
@@ -0,0 +1,146 @@
+#include "PmergeMe.hpp"
+
+template<typename T>
+void PmergeMe::mergeInsertionSort(T& container, int groupSize)
+{
+    typedef typename T::iterator Iterator;
+
+    int numOfGroups = container.size() / groupSize;
+    if (numOfGroups < 2)
+        return;
+
+    bool isOdd = numOfGroups % 2 == 1; // Number of groups is odd
+    Iterator lastGroup = next(container.begin(), groupSize * numOfGroups); // End iterator of last group
+    Iterator end = next(lastGroup, -(isOdd * groupSize)); // Start iterator of last group
+
+    sortGroups(container, end, groupSize);
+    mergeInsertionSort(container, groupSize * 2);
+
+    std::vector<Iterator> main, pend;
+    initMainAndPend(container, main, pend, groupSize, numOfGroups);
+    insertJacobsthalGroups<Iterator>(main, pend);
+    insertRemainingPend<Iterator>(main, pend, isOdd);
+    reconstructSortedContainer<Iterator>(container, main, groupSize);
+}
+
+template<typename T, typename Iterator>
+void PmergeMe::sortGroups(T& container, Iterator& end, int groupSize)
+{
+    int step = 2 * groupSize;
+    for (Iterator it = container.begin(); it != end; std::advance(it, step)) {
+        Iterator a = next(it, groupSize - 1);
+        Iterator b = next(it, groupSize * 2 - 1);
+        if (!compare(a, b)) // Number of comparisons is always once per group for each recursion
+            swapElements(a, groupSize);
+    }
+}
+
+template<typename T, typename ItVec>
+void PmergeMe::initMainAndPend(T& container, ItVec& main, ItVec& pend, int groupSize, int numOfGroups)
+{
+    main.push_back(next(container.begin(), groupSize - 1)); // Push b1
+    main.push_back(next(container.begin(), groupSize * 2 - 1)); // Push a1
+
+    for (int i = 3; i <= numOfGroups; i++) {
+        if (i % 2 == 1)
+            pend.push_back(next(container.begin(), groupSize * i - 1));
+        else
+            main.push_back(next(container.begin(), groupSize * i - 1));
+    }
+}
+
+template<typename Iterator, typename ItVec>
+void PmergeMe::insertJacobsthalGroups(ItVec& main, ItVec& pend)
+{
+    int prevJacobsthal = jacobsthalNumber(1);
+    // insertedNumbers only updates after all of the numbers from each group
+    // has been inserted. Used to define the boundary for insertion
+    int insertedNumbers = 0;
+    for (int k = 2;; k++) {
+        int currJacobsthal = jacobsthalNumber(k);
+        int jacobsthalDiff = currJacobsthal - prevJacobsthal;
+        // Break if remaining number of elements in pend is less than the jacobsthalDiff
+        if (jacobsthalDiff > static_cast<int>(pend.size()))
+            break;
+        // pendIt is the end Iterator of the current group
+        typename ItVec::iterator pendIt = next(pend.begin(), jacobsthalDiff - 1);
+        // boundIt is the bound for the Binary Insertion in the main chain
+        typename ItVec::iterator boundIt = next(main.begin(), currJacobsthal + insertedNumbers);
+        int offset = 0;
+        for (int nbrOfInsertions = 0; nbrOfInsertions < jacobsthalDiff; nbrOfInsertions++) {
+            //upper_bound uses Binary Insertion, passing it the compare function overloads the comparator
+            typename ItVec::iterator idx = std::upper_bound(main.begin(), boundIt, *pendIt, compare<Iterator>);
+            typename ItVec::iterator inserted = main.insert(idx, *pendIt);
+            pend.erase(pendIt); // Remove the inserted iterator from pend
+            std::advance(pendIt, -1); // Move the iterator backwards
+            offset += (inserted - main.begin()) == currJacobsthal + insertedNumbers;
+            boundIt = next(main.begin(), currJacobsthal + insertedNumbers - offset);
+        }
+        prevJacobsthal = currJacobsthal;
+        insertedNumbers += jacobsthalDiff;
+    }
+}
+
+template<typename Iterator, typename ItVec>
+void PmergeMe::insertRemainingPend(ItVec& main, ItVec& pend, bool isOdd)
+{
+    for (int i = pend.size() - 1; i >= 0; i--) {
+        typename std::vector<Iterator>::iterator currPend = next(pend.begin(), i);
+        typename std::vector<Iterator>::iterator currBound = next(main.begin(), main.size() - pend.size() + i + isOdd);
+        typename std::vector<Iterator>::iterator idx = std::upper_bound(main.begin(), currBound, *currPend, compare<Iterator>);
+        main.insert(idx, *currPend);
+    }
+}
+
+template<typename Iterator, typename T, typename ItVec>
+void PmergeMe::reconstructSortedContainer(T& container, ItVec& main, int groupSize)
+{
+    std::vector<int> copy;
+    copy.reserve(container.size());
+    for (typename std::vector<Iterator>::iterator it = main.begin(); it != main.end(); it++) {
+        for (int i = 0; i < groupSize; i++) {
+            Iterator pairStart = *it;
+            std::advance(pairStart, -groupSize + i + 1);
+            copy.insert(copy.end(), *pairStart);
+        }
+    }
+    std::copy(copy.begin(), copy.end(), container.begin());
+}
+
+template<typename T>
+void printContainer(T& container)
+{
+    typename T::iterator lastComma = --container.end();
+
+    std::cout << "{";
+    for (typename T::iterator it = container.begin(); it != container.end(); ++it) {
+        std::cout << *it;
+        if (it != lastComma)
+            std::cout << ",";
+    }
+    std::cout << "}\n";
+}
+
+template<typename T>
+void PmergeMe::swapElements(T it, int groupSize)
+{
+    T start = next(it, -groupSize + 1);
+    T end = next(start, groupSize);
+    while (start != end) {
+        std::iter_swap(start, next(start, groupSize));
+        start++;
+    }
+}
+
+template<typename T>
+T PmergeMe::next(T it, int steps)
+{
+    std::advance(it, steps);
+    return it;
+}
+
+template<typename T>
+bool PmergeMe::compare(T a, T b)
+{
+    return *a < *b;
+}
